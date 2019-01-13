868ddac4872bd73ea9ee27e50ba3d6c3250d305b
mayukoro <vc.mii.tantan@gmail.com>
2019-01-13 14:59:42 +0900

    Updates

---
+++ precache-manifest.afcc3b142576ad446f01d785849dabc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Mayuko-App/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1184609f7ba7c397bf05",
+    "url": "/Mayuko-App/static/js/runtime~main.1184609f.js"
+  },
+  {
+    "revision": "4c20bbf9d3548e4c00a0",
+    "url": "/Mayuko-App/static/js/main.4c20bbf9.chunk.js"
+  },
+  {
+    "revision": "0a7a1053aaedd785abb5",
+    "url": "/Mayuko-App/static/js/1.0a7a1053.chunk.js"
+  },
+  {
+    "revision": "4c20bbf9d3548e4c00a0",
+    "url": "/Mayuko-App/static/css/main.5431b008.chunk.css"
+  },
+  {
+    "revision": "71a4d295832a709fcdbac7bafdceadd9",
+    "url": "/Mayuko-App/index.html"
+  }
+];
